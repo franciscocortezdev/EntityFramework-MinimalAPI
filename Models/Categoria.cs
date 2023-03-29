cec158efbf1a2683aedbcde7e2a52b7c26d6df7a
Francisco Cortez <franciscocortezdev@gmail.com>
2023-03-29 15:25:20 -0600

    Model category

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -3,15 +3,11 @@ using System.Text.Json.Serialization;
 
 namespace CursoEntityFramework.Models;
 
-public class Categoria{
-  public Guid CategoriaId {get; set;}
+public class Category{
+  public int Id {get; set;}
 
+  public string? Name {get; set;}
+  public string? Description {get; set;}
 
-  public string? Nombre {get; set;}
-  public string? Descripcion {get; set;}
-
-  public int Peso {get;set;}
-  [JsonIgnore]
-  public virtual ICollection<Tarea>? Tareas {get; set;}
 
 }
